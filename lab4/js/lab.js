4f49d86658e3c4bd211b203956aaec8b7c4019fd
Kayla-leee <kalee@csumb.edu>
2020-09-13 23:36:20 -0700

    updated lab 4

---
+++ lab4/js/lab.js
@@ -0,0 +1,16 @@
+// How to Shuffle a Deck of Cards (Riffle Shuffle)
+  // Split the deck into two equal piles.
+	// Take one pile into each hand.
+	// Hold the base of the piles with two fingers, and hold the tops with your thumbs.
+	// Bend the piles with your fingers and thumbs, and then release your thumbs, letting the cards move and pile on top of each other.
+	// Combine piles and start again, if needed.
+
+
+
+// Creating a Rock-Paper-Scissors Game
+  // Create choices: rock, paper, and scissors.
+	// Create an opponent with randomized choices.
+	// Allow user to make a choice.
+	// Allow computer to make a choice.
+	// Create a scenario where the user wins.
+	// Test.
