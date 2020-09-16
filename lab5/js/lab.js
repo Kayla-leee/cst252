0de376a63cd9639f563b60082ad8e2f85bfb5516
Kayla-leee <kalee@csumb.edu>
2020-09-15 23:23:09 -0700

    did lab 5

---
+++ lab5/js/lab.js
@@ -0,0 +1,15 @@
+// Variables:
+make = "Mazda 3";
+model = "Touring";
+color = "Deep Crystal Blue Mica";
+year = 2018;
+
+// Calculate
+age = 2020 - year;
+
+// Output
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Age: " + age + " years<br>");
