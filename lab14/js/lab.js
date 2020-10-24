96ad2ee38b62f96e4de89e33e87de1a91e04fbaa
Kayla-leee <kalee@csumb.edu>
2020-10-23 22:56:12 -0700

    did lab 14

---
+++ lab14/js/lab.js
@@ -0,0 +1,28 @@
+/*
+ * Author: Kayla Lee
+ * Created: October 23, 2020
+ * License: Public Domain
+ */
+
+
+// constructor for vehicle
+
+class Vehicle {
+	constructor (make, model, year, color) {
+		this.carmake = make;
+		this.carmodel = model;
+		this.caryear = year;
+		this.carcolor = color;
+	}
+	info() {
+		return "I don't have, but want a " + this.carcolor + " " + this.caryear + " " +
+		this.carmake + " " + this.carmodel + "."
+	}
+}
+
+var car = new Vehicle("Chevy", "Malibu", 2020, "silver ice metallic");
+console.log(car.info());
+
+// we add text (including <p> tags to the end of our output div
+document.getElementById("output").innerHTML = "<p>" + car.info() + "</p>";
+
